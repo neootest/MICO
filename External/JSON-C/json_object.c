406e21fc88f8624ac27a7452b9a6e11c16d0f7f7
Jerry Yu <jerryyu001@users.noreply.github.com>
2015-01-30 16:48:07 +0800

    delete unused demos

--- External/JSON-C/json_object.c
+++ External/JSON-C/json_object.c
@@ -69,14 +69,7 @@ static void json_object_fini(void) {
   lh_table_free(json_object_table);
 }
 #endif /* REFCOUNT_DEBUG */
-#if defined(__CC_ARM)
-char* strdup(const char* str){
-    char* ptr;
-    ptr = malloc(strlen(str)+1);
-    strcpy(ptr,str);
-    return ptr;
-}
-#endif
+
 
 /* string escaping */
 
--- Platform/Common/Cortex-M3/STM32F2xx/stm32f2xx_platform.c
+++ Platform/Common/Cortex-M3/STM32F2xx/stm32f2xx_platform.c
@@ -46,11 +46,6 @@
 #include "../../GCC/stdio_newlib.h"
 #endif /* ifdef __GNUC__ */
 
-#ifdef __GNUC__
-#define WEAK __attribute__ ((weak))
-#elif defined ( __IAR_SYSTEMS_ICC__ )
-#define WEAK __weak
-#endif /* ifdef __GNUC__ */
 
 /******************************************************
 *                      Macros
